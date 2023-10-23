639726d904c4c3283bbe2b0f42cd37ccf5912ddb
dtyunegou <dtyunegov@mail.ru>
2023-10-23 15:49:49 +0500

    module2-task1: FavoritesPage - вынесены компоненты

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,5 +1,7 @@
+import Container from '../../components/common/Container';
 import Footer from '../../components/common/Footer';
 import Header from '../../components/common/Header';
+import PageMain from '../../components/common/Page';
 import FavoritesPlace from '../../components/favorites/FavoritesPlace';
 import { NiceCard, WhiteCastleCard, WoodAndStoneCard } from '../../constants/Cards';
 import FavoritePlace from '../../types/FavoritePlace';
@@ -17,17 +19,23 @@ const Cologne: FavoritePlace = {
 const Favorites = () => (
   <div className="page">
     <Header/>
-    <main className="page__main page__main--favorites">
-      <div className="page__favorites-container container">
-        <section className="favorites">
-          <h1 className="favorites__title">Saved listing</h1>
-          <ul className="favorites__list">
-            <FavoritesPlace place={ Amsterdam }/>
-            <FavoritesPlace place={ Cologne }/>
-          </ul>
-        </section>
-      </div>
-    </main>
+    <PageMain
+      className="page__main page__main--favorites"
+      content={
+        <Container
+          className='page__favorites-container container'
+          content={
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                <FavoritesPlace place={Amsterdam} />
+                <FavoritesPlace place={Cologne} />
+              </ul>
+            </section>
+          }
+        />
+      }
+    />
     <Footer/>
   </div>
 );
