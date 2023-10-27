0b4e67efad1b7628ca2a7385ea26c4ea92c6601c
dtyunegou <dtyunegov@mail.ru>
2023-10-27 19:28:44 +0500

    module3-task1: страница не найдена

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -6,7 +6,7 @@ const MainEmpty = () => (
     <Page className="page__main--index page__main--index-empty">
       <Container>
         <h1>Страница не найдена</h1>
-        <img src='markup/img/not_found.png' alt='Страница не найдена'></img>
+        <img src="markup/img/not_found.png" alt="Страница не найдена"></img>
       </Container>
     </Page>
   </div>
